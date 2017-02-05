84978a8f30ea48f1d9b4dce2ea62d9f7781f4608
qiaoqiaoli <joyinbritish@gmail.com>
2017-02-05 23:12:25 +0100

    step 1 completed, trying to translates from key skema

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var AWS = require('aws-sdk')
 AWS.config.update({
     accessKeyId: "AKIAINLKNRAYC4TGXD2A",
     secretAccessKey: "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",
-    "region": "eu-west-1"
+    "region": "eu-west-1",
 })
 
 var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'})
@@ -17,7 +17,6 @@ var lowerbound = data.lowerbound
 var upperbound = data.upperbound
 var order = data.order
 var results = []
-
 
 function getLowerBound(tableName) {
 
@@ -69,9 +68,12 @@ function getSecret(tableName) {
 	})	
 }
 
+/* Step 1: 
+ * Retrieve all characters within lower and upper bounds.
+ */
 function getFilteredCharacters(tableName) {
 	dynamodb.scan({
-	// Step 1: Retrieve all characters within lower and upper bounds
+
 	    TableName : tableName
 	}, (err, data) => {
 	    if (err) {
@@ -83,14 +85,23 @@ function getFilteredCharacters(tableName) {
 			}
 		})
 
-		// Step 2: Use the  from  and  to  mapping
+	   	// console.log(results)
+		console.log(results.length)		// 793
+
 		mapping(results, order)
 	})	
 }
 
-/* Step 3: Use the from and to mapping in the application.order table to 
+/* Step 2: 
+ * Use the from and to mapping in the application.order table to 
  * transform the results from the previous step into the right order.
  */
+
+//     KeySchema: [       
+//         { AttributeName: "id", KeyType: "HASH"},  //Partition key
+//         { AttributeName: "number", KeyType: "RANGE" }  //Sort key
+//     ],
+
 function mapping(results, order) {
 	results.forEach( (result) => {
 		order.forEach( (ord) => {
@@ -107,8 +118,54 @@ function mapping(results, order) {
 	// }
 }
 
+/* Step 3: 
+ * Get the secret number by using the constructed key from step 2 on application.secret
+ */
+
+/* Step 4:
+ * Divide the retrieved secret number by the amount of objects retrieved in question 1
+ * multiplied by the consumed read capacity for that search operation.
+ */
+
+//     ProvisionedThroughput: {       
+//         ReadCapacityUnits: 10, 
+//         WriteCapacityUnits: 10
+//     }
+
+
+/* Step 5: Write your result to the application.results table. Use your own name as key and
+ * submit as attribute to the answer field.
+ */
+
+function writeToTable() {
+	var docClient = new AWS.DynamoDB.DocumentClient();
+
+	var table = "application.results";
+
+	var name = 'qiaoqiao';
+	var answer = results;
+
+	var params = {
+	    TableName:table,
+	    Item:{
+	        "name": name,
+	        "answer": answer
+	    }
+	};
+
+	console.log("Adding a new item...");
+	docClient.put(params, function(err, data) {
+	    if (err) {
+	        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+	    } else {
+	        console.log("Added item:", JSON.stringify(data, null, 2));
+	    }
+	})
+}
+
 getLowerBound("application.low")
 getUpperBound("application.high")
 getFilteredCharacters("application.characters")
 getOrder("application.order")
 getSecret("application.secret")
+// writeToTable()
